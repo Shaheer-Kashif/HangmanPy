d877205cac6e2bfe70f1f3ef718ecf3dcafaccdf
Shaheer Kashif <codesshaheer@gmail.com>
2024-07-30 13:03:09 +0500

    Final Build

--- main.py
+++ main.py
@@ -9,9 +9,20 @@ root = Tk()
 root.config(padx=10)
 root.title("Hangman")
 
-# Loading Relevant Images
+# Loading Hangman Icon
 root.iconbitmap("icons/hangman.ico")
 
+window_width = 380
+window_height = 300
+
+# Set the window size
+root.geometry(f"{window_width}x{window_height}")
+
+# Lock the window size by setting minsize and maxsize to the same value
+root.minsize(window_width, window_height)
+root.maxsize(window_width, window_height)
+
+# Loading all Relevant Images
 logo = ImageTk.PhotoImage(Image.open("icons/hangman.ico").resize((100,100)))
 
 top_frame = Frame(root)
@@ -92,23 +103,26 @@ def animate1(path):
     
 def animate2(count,path):
     global showAnimation
-    if path == "reset":
-        root.after_cancel(showAnimation)
-        image_placeholder.config(image=default_image)
-    else:
-        newImage = imageObject[count]
-        
-        image_placeholder.configure(image=newImage)
-        count += 1
-        
-        if path == "media/win.gif" or path == "media/loose.gif":
-            if count == frames:
-                count = 0
-            showAnimation = root.after(50, lambda: animate2(count,path))
-        
+    try:
+        if path == "reset":
+            root.after_cancel(showAnimation)
+            image_placeholder.config(image=default_image)
         else:
-            if count < frames:
+            newImage = imageObject[count]
+            
+            image_placeholder.configure(image=newImage)
+            count += 1
+            
+            if path == "media/win.gif" or path == "media/loose.gif":
+                if count == frames:
+                    count = 0
                 showAnimation = root.after(50, lambda: animate2(count,path))
+            
+            else:
+                if count < frames:
+                    showAnimation = root.after(50, lambda: animate2(count,path))
+    except:
+        pass
     
           
 # Certain SFX Playing Function  
@@ -194,133 +208,164 @@ def wordcheck(guessed_letter):
 # Main Screen Load
 def play():
     global game_window,credits_window,stats_window,lives,category,hidden_word,word,word_label,lives_text,game_window,keyboard,default_image,image_placeholder,hint,hint_button,button_frame,replay_menu,replay_text,word
+    
     try:
-        if stats_window.winfo_exists():
-            stats_window.destroy()
-        if credits_window.winfo_exists():
-            credits_window.destroy()
+        if game_window.winfo_exists():
+            messagebox.showerror("Error","An another instance of the application is already running.")
+        raise Exception
     except:
+ 
+        # Check if Stats and Credits Windows are Opened.
         try:
+            if stats_window.winfo_exists():
+                stats_window.destroy()
             if credits_window.winfo_exists():
                 credits_window.destroy()
         except:
+            try:
+                if credits_window.winfo_exists():
+                    credits_window.destroy()
+            except:
+                pass
             pass
-        pass
-    
-    try:
-        api_req = requests.get("https://www.wordgamedb.com/api/v1/words/random")
-    except:
-        messagebox.showerror("Error","There was an issue launching the application, maybe check your Internet.")
-        exit()
         
-    api = json.loads(api_req.content)
-    word = api['word']
-    hint = api['hint']
-    category = api['category']
-    
-    lives = 6
-    
-    game_window = Toplevel()
-    
-    mus_check = threading.Thread(target=eventcheckbgmusic)
-    mus_check.start()
-    
-    hangman = LabelFrame(game_window,width=600,height=200,border=0)
-    hangman.grid(row=0,column=0,rowspan=3)
-    
-    image_placeholder = Label(hangman,image=default_image)
-    image_placeholder.pack()
-    
-    upperframe = LabelFrame(game_window,width=350,height=100,padx=100,border=0)
-    upperframe.grid_propagate(0)
-    upperframe.grid(row=0,column=1)
-    
-    hidden_word = ["_" for letter in word]
-
-    word_label = Label(upperframe,text=hidden_word,font=("Montserrat",18,"bold"))
-    word_label.grid(row=1,column=0,columnspan=2)
-    
-    category_frame = LabelFrame(upperframe,border=0)
-    category_frame.grid(row=2,column=0,columnspan=2)
-    
-    category_label = Label(category_frame,text="Category: ",font=("Montserrat",13,"bold"))
-    category_label2 = Label(category_frame,text=category,font=("Montserrat",12))
-    
-    category_label.grid(row=0,column=0)
-    category_label2.grid(row=0,column=1)
-    
-    lives_frame = LabelFrame(upperframe,border=0)
-    lives_frame.grid(row=0,column=0,columnspan=2)
-    
-    lives_label = Label(lives_frame,image=lives_image)
-    lives_label.grid(row=0,column=0)
-    
-    lives_text = Label(lives_frame,text=lives,font=("Montserrat",12,"bold"))
-    lives_text.grid(row=0,column=1)
-    
-    button_frame = LabelFrame(game_window,border=0,height=50,width=250)
-    button_frame.grid(row=1,column=1,pady=10)
-    
-    hint_button = Button(button_frame,image=hint_image,border=0,command=lambda: func_buttons('hint'), height=50)
-    hint_button.grid(row=0,column=0,padx=10)
-    
-    replay_button = Button(button_frame,image=replay_button_image,border=0,command=lambda: func_buttons('replay'))
-    replay_button.grid(row=0,column=1,padx=10)
-    
-    quit_button = Button(button_frame,image=quit_button_image,border=0,command=lambda: func_buttons('quit'))
-    quit_button.grid(row=0,column=2,padx=10)
-    
-    keyboard = LabelFrame(game_window,border=0)
-    keyboard.grid(row=2,column=1)
-    
-    replay_menu = LabelFrame(game_window,border=0)
-    replay_menu.grid(row=3,column=0,columnspan=2,pady=10)
+        # Check if APi Loads otherwise return an error
+        try:
+            api_req = requests.get("https://www.wordgamedb.com/api/v1/words/random")
+        except:
+            messagebox.showerror("Error","There was an issue launching the application, maybe check your Internet?")
+            exit()
             
-    replay_text = Label(replay_menu)
-    replay_text.grid(row=0,column=0,columnspan=2)
-    
-    temp = Button(keyboard,text="  ",width=3,border=0,background="#27aae1",foreground="white",font="montserrat")
-    temp.grid(row=3,column=1)
-    
-    for i in range(26):
-        letter = chr(ord('a') + i)
-        globals()["button_"+letter] = Button(keyboard,text=letter.upper(),command=lambda le = letter: wordcheck(le),width=3,border=0,background="#27aae1",foreground="white",font="montserrat")
         
-        if i >= 21:
-            globals()["button_"+letter].grid(row=(i+1)//7,column=(i+1)%7,padx=5,pady=5)
-        else:
-            globals()["button_"+letter].grid(row=i//7,column=i%7,padx=5,pady=5)
+        api = json.loads(api_req.content)
+        word = api['word']
+        hint = api['hint']
+        category = api['category']
+        
+        lives = 6
+        
+        game_window = Toplevel()
+        
+        game_window_width = 720
+        game_window_height = 430
+
+        # Set the window size
+        game_window.geometry(f"{game_window_width}x{game_window_height}")
+
+        # Lock the window size by setting minsize and maxsize to the same value
+        game_window.minsize(game_window_width, game_window_height)
+        game_window.maxsize(game_window_width, game_window_height)
+        
+        mus_check = threading.Thread(target=eventcheckbgmusic)
+        mus_check.start()
+        
+        hangman = LabelFrame(game_window,width=600,height=200,border=0)
+        hangman.grid(row=0,column=0,rowspan=3)
+        
+        image_placeholder = Label(hangman,image=default_image)
+        image_placeholder.pack()
+        
+        upperframe = LabelFrame(game_window,width=350,height=100,padx=100,border=0)
+        upperframe.grid_propagate(0)
+        upperframe.grid(row=0,column=1)
+        
+        hidden_word = ["_" for letter in word]
+
+        word_label = Label(upperframe,text=hidden_word,font=("Montserrat",18,"bold"))
+        word_label.grid(row=1,column=0,columnspan=2)
+        
+        category_frame = LabelFrame(upperframe,border=0)
+        category_frame.grid(row=2,column=0,columnspan=2)
+        
+        category_label = Label(category_frame,text="Category: ",font=("Montserrat",13,"bold"))
+        category_label2 = Label(category_frame,text=category,font=("Montserrat",12))
+        
+        category_label.grid(row=0,column=0)
+        category_label2.grid(row=0,column=1)
+        
+        lives_frame = LabelFrame(upperframe,border=0)
+        lives_frame.grid(row=0,column=0,columnspan=2)
+        
+        lives_label = Label(lives_frame,image=lives_image)
+        lives_label.grid(row=0,column=0)
+        
+        lives_text = Label(lives_frame,text=lives,font=("Montserrat",12,"bold"))
+        lives_text.grid(row=0,column=1)
+        
+        button_frame = LabelFrame(game_window,border=0,height=50,width=250)
+        button_frame.grid(row=1,column=1,pady=10)
+        
+        hint_button = Button(button_frame,image=hint_image,border=0,command=lambda: func_buttons('hint'), height=50)
+        hint_button.grid(row=0,column=0,padx=10)
+        
+        replay_button = Button(button_frame,image=replay_button_image,border=0,command=lambda: func_buttons('replay'))
+        replay_button.grid(row=0,column=1,padx=10)
+        
+        quit_button = Button(button_frame,image=quit_button_image,border=0,command=lambda: func_buttons('quit'))
+        quit_button.grid(row=0,column=2,padx=10)
+        
+        keyboard = LabelFrame(game_window,border=0)
+        keyboard.grid(row=2,column=1)
+        
+        replay_menu = LabelFrame(game_window,border=0)
+        replay_menu.grid(row=3,column=0,columnspan=2,pady=10)
+                
+        replay_text = Label(replay_menu)
+        replay_text.grid(row=0,column=0,columnspan=2)
+        
+        temp = Button(keyboard,text="  ",width=3,border=0,background="#27aae1",foreground="white",font="montserrat")
+        temp.grid(row=3,column=1)
+        
+        for i in range(26):
+            letter = chr(ord('a') + i)
+            globals()["button_"+letter] = Button(keyboard,text=letter.upper(),command=lambda le = letter: wordcheck(le),width=3,border=0,background="#27aae1",foreground="white",font="montserrat")
+            
+            if i >= 21:
+                globals()["button_"+letter].grid(row=(i+1)//7,column=(i+1)%7,padx=5,pady=5)
+            else:
+                globals()["button_"+letter].grid(row=i//7,column=i%7,padx=5,pady=5)
             
 # Background Music Playing Function
 def eventcheckbgmusic():
     global game_window,transition_game,transition_menu
     
-    while True:
-        if game_window.winfo_exists():
-            if transition_game == False:
-                for step in range(steps):
-                    new_volume = initial_volume - (initial_volume - final_volume) * (step + 1) / steps
-                    bg_music.set_volume(new_volume)
-                    time.sleep(step_duration)
-                transition_game = True
-                transition_menu = False
-        else:
-            if transition_menu == False:
-                for step in range(steps):
-                    new_volume = final_volume - (final_volume - initial_volume) * (step + 1) / steps
-                    bg_music.set_volume(new_volume)
-                    time.sleep(step_duration)
-                
-                transition_menu = True
-                transition_game = False
-        time.sleep(.1)
+    try:
+        while True:
+            if game_window.winfo_exists():
+                if transition_game == False:
+                    for step in range(steps):
+                        new_volume = initial_volume - (initial_volume - final_volume) * (step + 1) / steps
+                        bg_music.set_volume(new_volume)
+                        time.sleep(step_duration)
+                    transition_game = True
+                    transition_menu = False
+            else:
+                if transition_menu == False:
+                    for step in range(steps):
+                        new_volume = final_volume - (final_volume - initial_volume) * (step + 1) / steps
+                        bg_music.set_volume(new_volume)
+                        time.sleep(step_duration)
+                    
+                    transition_menu = True
+                    transition_game = False
+            time.sleep(.1)
+    except:
+        pass
     
-        
-
 # Stats Menu
 def stats_menu():
     global stats_window
     stats_window = Toplevel(padx=10,pady=10)
+    
+    stats_window_width = 215
+    stats_window_height = 180
+
+    # Set the window size
+    stats_window.geometry(f"{stats_window_width}x{stats_window_height}")
+
+    # Lock the window size by setting minsize and maxsize to the same value
+    stats_window.minsize(stats_window_width, stats_window_height)
+    stats_window.maxsize(stats_window_width, stats_window_height)
     
     heading = Label(stats_window,text="All Time Stats",font=("Montserrat",20,"bold"),foreground="#27aae1")
     heading.grid(row=0,column=0,columnspan=2)
@@ -353,6 +398,16 @@ def credits_menu():
     global credits_window
     credits_window = Toplevel(padx=10,pady=10)
     
+    credits_window_width = 270
+    credits_window_height = 100
+
+    # Set the window size
+    credits_window.geometry(f"{credits_window_width}x{credits_window_height}")
+
+    # Lock the window size by setting minsize and maxsize to the same value
+    credits_window.minsize(credits_window_width, credits_window_height)
+    credits_window.maxsize(credits_window_width, credits_window_height)
+    
     heading = Label(credits_window,text="Made By",font=("Montserrat",20,"bold"),foreground="#27aae1")
     heading.grid(row=0,column=0,columnspan=2)
     
@@ -378,7 +433,6 @@ LogoWindow.pack(side="left")
 Logo = Label(LogoWindow, image=logo)
 Logo.grid(row=0,column=0,padx=15)
 
-
 LogoText = Frame(top_frame,borderwidth=0)
 LogoText.pack(side="left")
 
@@ -387,7 +441,6 @@ GameName.grid(row=0,column=0)
 
 Slogan = Label(LogoText,text="Can you save the poor soul?",font=("Montserrat",11))
 Slogan.grid(row=1,column=0)
-    
     
 ButtonWindow = Frame(root,borderwidth=1)
 ButtonWindow.pack(fill="both", expand=True)
